e0663ae364e25127056669a69f448ff5e86bd686
Igor <sergienko339@gmail.com>
2024-03-26 01:32:06 +0200

    added hashrouter

--- src/modules/router/router.tsx
+++ src/modules/router/router.tsx
@@ -1,7 +1,11 @@
 import { Preview } from 'modules/pages/Preview/Preview';
 import { Me } from 'modules/pages/Me/Me';
 import { MyJobs } from 'modules/pages/MyJobs/MyJobs';
-import { createBrowserRouter, Navigate } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  Navigate,
+  createHashRouter,
+} from 'react-router-dom';
 import { ReachMe } from 'modules/pages/ReachMe/ReachMe';
 
 export const routes = [
@@ -12,6 +16,6 @@ export const routes = [
   { path: 'reachMe', element: <ReachMe /> },
 ];
 
-const router = createBrowserRouter(routes);
+const router = createHashRouter(routes);
 
 export default router;
